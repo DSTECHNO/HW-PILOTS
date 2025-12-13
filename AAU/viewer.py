09d2758f86668af61dd28214435a953c289330e4
Mustafa Kuzay <90314532+mkuzaay@users.noreply.github.com>
2025-12-13 19:16:55 +0300

    Update viewer.py

--- AAU/viewer.py
+++ AAU/viewer.py
@@ -7,6 +7,44 @@ from scipy.interpolate import griddata
 import os
 import requests
 
+# -------------------------------------------------
+# 1. PASSWORD AUTHENTICATION
+# -------------------------------------------------
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        # Checks whether a password entered by the user is correct.
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password in the session state for security
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Please enter the password:", 
+        type="password", 
+        on_change=password_entered, 
+        key="password"
+    )
+    
+    if "password_correct" in st.session_state and not st.session_state["password_correct"]:
+        st.error("😕 Password incorrect. Please try again.")
+
+    return False
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
+
+# -------------------------------------------------
+
 @st.cache_data(show_spinner=False)
 def ensure_file(url: str, local_path: str) -> str:
     os.makedirs(os.path.dirname(local_path) or ".", exist_ok=True)
