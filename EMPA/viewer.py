e17bf6d0798b52545c9faf899d266dfddb91614b
Mustafa Kuzay <90314532+mkuzaay@users.noreply.github.com>
2025-12-18 11:59:00 +0300

    Update viewer.py

--- EMPA/viewer.py
+++ EMPA/viewer.py
@@ -290,10 +290,10 @@ st.title("Thermal Twin for EMPA Pilot")
 
 HF_USER = "mkuzaay"  # örn: "DSTECHNO"
 
-NPZ_URL = f"https://huggingface.co/datasets/{HF_USER}/hw-pilots-data/resolve/main/validationCaseEMPA.npz"
+NPZ_URL = f"https://huggingface.co/datasets/{HF_USER}/hw-pilots-data/resolve/main/ValidEMPA.npz"
 VTK_URL = f"https://huggingface.co/datasets/{HF_USER}/hw-pilots-data/resolve/main/ValidEMPA.vtk"
 
-npz_path = ensure_file(NPZ_URL, "validationCaseEMPA.npz")
+npz_path = ensure_file(NPZ_URL, "ValidEMPA.npz")
 vtk_path = ensure_file(VTK_URL, "ValidEMPA.vtk")
 
 mesh, T_field, U_field = load_npz_case(npz_path, vtk_path)
